7e611517f30b6295829accfde03f20c9742e6194
None.ku <dev.nassim.pro@gmail.com>
2023-06-14 11:00:46 +0200

    Add function Notification and clear the code

--- 6-POO/Compte/execute.php
+++ 6-POO/Compte/execute.php
@@ -2,19 +2,26 @@
 include("./client.class.php");
 include("./compte.class.php");
 
+//Create Account - Client
+
 $client1 = new Client("512163AFGH", "Gacem", "Nassim", "062345871");
 $compte1 = new Compte(1000, $client1);
 
-echo($compte1->getCode());
-$client2 = new Client("512454HGHGGH", "Prout", "Ventilo", "062124871");
+
+$client2 = new Client("512454HGHGGH", "Levan", "Stay", "062124871");
 $compte2 = new Compte(1000, $client2);
 
 $client3 = new Client("51212346GGH", "Loli", "Kerp", "062874871");
 $compte3 = new Compte(1000, $client3);
 
+//Opération Lists
+
 echo "\n\n";
-$compte3->crediterCPT(300, $compte2);
-$compte3->debiter(100);
-$compte3->affichage();
+$compte2->crediter(600);
+$compte2->affichage();
+$compte2->crediterCPT(300, $compte3);
+// $compte2->debiter(100);
+// echo "\n\n";
+ $compte3->affichage();
 
 ?>
