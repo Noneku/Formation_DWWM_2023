a85fd283458312593da1ddcca5c9ec4bf1cee081
None.ku <dev.nassim.pro@gmail.com>
2023-06-15 10:41:15 +0200

    Create class Etudiant / Filiere

--- 6-POO/Etudiant/etudiant.class.php
+++ 6-POO/Etudiant/etudiant.class.php
@@ -1,5 +1,99 @@
 <?php
 
+class Etudiant {
+    
+    private int $id;
+    private string $nom;
+    private string $prenom;
+    private DateTime $dateNaissance;
+    
+    private Filiere $filiere;
 
+    private static int $nbrStudent = 0;
+
+    
+    public function __construct($nom, $prenom, $dateNaissance) {
+
+        $this->id = self::$nbrStudent++;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateNaissance = DateTime::createFromFormat('d/m/Y', $dateNaissance);
+
+    }
+
+    
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of nom
+     *
+     * @return string
+     */
+    public function getNom(): string {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @param string $nom
+     *
+     * @return self
+     */
+    public function setNom(string $nom): self {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     *
+     * @return string
+     */
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @param string $prenom
+     *
+     * @return self
+     */
+    public function setPrenom(string $prenom): self {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    /**
+     * Get the value of dateNaissance
+     *
+     * @return DateTime
+     */
+    public function getDateNaissance(): DateTime {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * Set the value of dateNaissance
+     *
+     * @param DateTime $dateNaissance
+     *
+     * @return self
+     */
+    public function setDateNaissance(DateTime $dateNaissance): self {
+        $this->dateNaissance = $dateNaissance;
+        return $this;
+    }
+}
 
 ?>
---
+++ 6-POO/Etudiant/filiere.class.php
@@ -0,0 +1,69 @@
+<?php
+
+
+class Filiere {
+    private int $id;
+    private string $code;
+    private string $libelle;
+    
+    private static int $nbrFiliere = 0;
+
+    
+    public function __construct($nom, $prenom, $libelle) {
+
+        $this->id = self::$nbrFiliere++;
+        $this->code = $nom;
+        $this->libelle = $prenom;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of code
+     *
+     * @return string
+     */
+    public function getCode(): string {
+        return $this->code;
+    }
+
+    /**
+     * Set the value of code
+     *
+     * @param string $code
+     *
+     * @return self
+     */
+    public function setCode(string $code): self {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * Get the value of libelle
+     *
+     * @return string
+     */
+    public function getLibelle(): string {
+        return $this->libelle;
+    }
+
+    /**
+     * Set the value of libelle
+     *
+     * @param string $libelle
+     *
+     * @return self
+     */
+    public function setLibelle(string $libelle): self {
+        $this->libelle = $libelle;
+        return $this;
+    }
+}
