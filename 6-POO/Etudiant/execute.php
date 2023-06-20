dc51b69175b403d58e5767aeae16323a484aaca3
Noneku <dev.nassim.pro@gmail.com>
2023-06-20 08:54:36 +0200

    Add new

--- 6-POO/Etudiant/execute.php
+++ 6-POO/Etudiant/execute.php
@@ -2,40 +2,37 @@
 include('./etudiant.class.php');
 include('./filiere.class.php');
 
-// $startProgram = readline("Selectionner le nombre d'étudiant");
+$startProgram = readline("Selectionner le nombre d'étudiant");
 $studentsLists = [];
 
-// for ($i=0; $i < $startProgram; $i++) { 
+for ($i=0; $i < $startProgram; $i++) { 
 
-//     $name = readline("Entrer le Nom");
-//     $prenom = readline("Entrer le Prenom");
-//     $dateNaissance = readline("Entrer le date de naissance");
-//     $code = readline("Entrer le Code");
-//     $libelle = readline("Entrer le libelle");
+    $name = readline("Entrer le Nom");
+    $prenom = readline("Entrer le Prenom");
+    $dateNaissance = readline("Entrer le date de naissance");
+    $code = readline("Entrer le Code");
+    $libelle = readline("Entrer le libelle");
 
-//     //C
-//     $etudiant = new Etudiant($name, $prenom, $dateNaissance, $code, $libelle);
-//     array_push($studentsLists, $etudiant);
-// }
+    //C
+    $etudiant = new Etudiant($name, $prenom, $dateNaissance, $code, $libelle);
+    array_push($studentsLists, $etudiant);
+}
 
 // $codeBranches = ["IT", "BC", "BTP","IG"];
 
-$etudiant1 = new Etudiant('Gacem','Nassim','31/07/1996','IT','Informatique');
-$etudiant2 = new Etudiant('Kaft','Kevin','21/06/1974','GC','Boucherie');
-$etudiant3 = new Etudiant('Seft','Lola','05/07/1992','GC','Batiment');
-$etudiant4 = new Etudiant('Ruft','Quentin','23/04/2002','COM','Ingenieurie');
-$etudiant5 = new Etudiant('Amar','Gest','23/04/1994','IT','Informatique');
+// $etudiant1 = new Etudiant('Gacem','Nassim','31/07/1996','IT','Informatique');
+// $etudiant2 = new Etudiant('Kaft','Kevin','21/06/1974','GC','Boucherie');
+// $etudiant3 = new Etudiant('Seft','Lola','05/07/1992','GC','Batiment');
+// $etudiant4 = new Etudiant('Ruft','Quentin','23/04/2002','COM','Ingenieurie');
+// $etudiant5 = new Etudiant('Amar','Gest','23/04/1994','IT','Informatique');
 
-$studentsLists = [
-    $etudiant1,
-    $etudiant2,
-    $etudiant3,
-    $etudiant4,
-    $etudiant5,
-];
+$studentsLists = [];
 
+$sortArray = [];
 
- $sortArray = [];
+$tempIT = [];
+$tempGC = [];
+$tempCOM = [];
 
 for ($i=0; $i < count($studentsLists); $i++) { 
 
@@ -70,7 +67,6 @@ $sortArray['Communication'] = $tempCOM;
 var_dump($sortArray['Informatique']);
 var_dump($sortArray['GénieCivil']);
 var_dump($sortArray['Communication']);
-
 ?>
 
 
