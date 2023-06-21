31702e6d4978bfdfa5351af155c6b8f9407cbcae
Nomu59 <114921870+Nomu59@users.noreply.github.com>
2023-06-21 13:44:40 +0200

    Delete 4-TP2 directory

--- 4-TP2/Ctrl/execute_saveTofile.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../Modele/functions.php";
-$studentsList = loadStudentsFromFile("../ListeStagiaire.txt");
-studentsToFile($studentsList);
-$studentsList = shufflePositions($studentsList);
-saveStudentsToCSVFile($studentsList);
-?>
