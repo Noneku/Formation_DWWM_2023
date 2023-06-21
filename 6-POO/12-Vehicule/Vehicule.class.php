ee58e9de0a92c95fb1c18eb87c5e966e1bf57b52
59013-96-09 <59013-96-09@exchange.ad.afpanet>
2023-06-21 14:17:36 +0200

    ajout CV

---
+++ 6-POO/12-Vehicule/Vehicule.class.php
@@ -0,0 +1,91 @@
+<?php
+
+abstract class Vehicule 
+
+{
+    private int $matricule;
+    private int $annee;
+    private float $prix;
+
+
+    public function __construct ($matricule, $annee, $prix) {
+        $this->matricule = $matricule;
+        $this->annee = $annee;
+        $this->prix = $prix;
+    }
+
+    public function demarrer () {
+
+    }
+
+    public function accelerer () {
+
+    }
+
+    
+
+
+
+    /**
+     * Get the value of matricule
+     */ 
+    public function getMatricule()
+    {
+        return $this->matricule;
+    }
+
+    /**
+     * Set the value of matricule
+     *
+     * @return  self
+     */ 
+    public function setMatricule($matricule)
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of annee
+     */ 
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+    /**
+     * Set the value of annee
+     *
+     * @return  self
+     */ 
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prix
+     */ 
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set the value of prix
+     *
+     * @return  self
+     */ 
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
+
+
+?>
