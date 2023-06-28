ce9b0068f021c10646769c8ee2d9b4fdd41728a0
Quentin Olivier <quentin.olivier6@outlook.fr>
2023-06-28 14:28:42 +0200

    maj des dossiers et requêtes SQL

--- 8-SQL/requete_SQL.sql
+++ 8-SQL/requete_SQL.sql
@@ -37,9 +37,7 @@ SELECT * from emp where comm is not null order by comm ASC;
 
 /* 15 : Sélectionner les employés qui n’ont pas de chef.*/
 
-
-
-
+SELECT * from emp where sup is NULL;
 
 
 /*17 : Sélectionner les vendeurs du service 6 qui ont un revenu mensuel supérieur ou
@@ -172,6 +170,7 @@ where noserv not in (select distinct noserv from emp);
 
 
 /*54 : Sélectionner les services ayant au moins un employé.*/
+
 SELECT service from serv s where noserv in (select distinct noserv from emp);
 
 
@@ -259,6 +258,9 @@ INNER JOIN (
 WHERE e.sal > avg_sal.salaire_moyen
 ORDER BY e.noserv ASC;
 
+
+
+
 /*68 : Sélectionner les employés du service INFORMATIQUE embauchés la même année qu'un employé du service VENTES.*/
 SELECT e.*
 FROM emp e
@@ -272,7 +274,15 @@ AND YEAR(e.embauche) IN (
 );
 
 
+
+
+
 /*69 : Sélectionner le nom, l’emploi, la ville pour les employés qui ne travaillent pas dans le même service que leur supérieur hiérarchique direct.*/
+
+SELECT employé.nom , employé.emploi , s.ville 
+from  emp employé , serv s 
+where employé.noserv = s.noserv and employé.noserv <> (SELECT superieur.noserv from emp superieur where employé.sup = superieur.noemp);
+
 
 /*70 : Sélectionner le nom, le prénom, le service, le revenu des employés qui ont des
 subalternes, trier le résultat suivant le revenu décroissant.*/
@@ -309,22 +319,30 @@ where e.emploi = "VENDEUR" and e.noserv = s.noserv;
 
 /*75 : Sélectionner nom, prénom, emploi, salaire, commissions, revenu mensuel pour les employés des services 3,5,6 */
 
-SELECT nom , prenom , emploi , sal , COALESCE(comm , 0) as comm , Round((sal + COALESCE(comm , 0)),2) as revenu_mensuel from emp e where e.noserv IN(3 , 5 , 6);
+SELECT nom , prenom , emploi , sal , COALESCE(comm , 0) as comm , Round((sal + COALESCE(comm , 0)),2) as revenu_mensuel 
+from emp e 
+where e.noserv IN(3 , 5 , 6);
 
 /*76 : Idem pour les employés des services 3,5,6 en remplaçant les noms des colonnes : SAL par SALAIRE, COMM par COMMISSIONS, SAL+IFNULL(COMM,0) par GAIN_MENSUEL.*/
 
-SELECT nom , prenom , emploi , sal SALAIRE , COALESCE(comm , 0) as COMMISSIONS , Round((sal + COALESCE(comm , 0)),2) GAIN_MENSUEL from emp e where e.noserv IN(3 , 5 , 6);
+SELECT nom , prenom , emploi , sal SALAIRE , COALESCE(comm , 0) as COMMISSIONS , Round((sal + COALESCE(comm , 0)),2) GAIN_MENSUEL 
+from emp e 
+where e.noserv IN(3 , 5 , 6);
 
 
 
 /*77 : Idem pour les employés des services 3,5,6 en remplaçant GAIN_ MENSUEL par GAINMENSUEL*/
 
-SELECT nom , prenom , emploi , sal SALAIRE , COALESCE(comm , 0) as COMMISSIONS , Round((sal + COALESCE(comm , 0)),2) GAINMENSUEL from emp e where e.noserv IN(3 , 5 , 6);
+SELECT nom , prenom , emploi , sal SALAIRE , COALESCE(comm , 0) as COMMISSIONS , Round((sal + COALESCE(comm , 0)),2) GAINMENSUEL 
+from emp e 
+where e.noserv IN(3 , 5 , 6);
 
 
 /*78 : Afficher le nom, l'emploi, les salaires journalier et horaire pour les employés des services 3,5,6 (22 jours/mois et 8 heures/jour), sans arrondi, arrondi au centime près.*/
 
-SELECT nom , emploi , round(((sal / 12) / 8), 2) salaire_journalier from emp e  where e.noserv IN(3 , 5 , 6);
+SELECT nom , emploi , round(((sal / 12) / 8), 2) salaire_journalier 
+from emp e  
+where e.noserv IN(3 , 5 , 6);
 
 
 /*79 : Idem sans arrondir mais en tronquant.*/
@@ -335,5 +353,257 @@ from emp e
  
  /*80 : Concaténer les colonnes Service et Ville en les reliant par " ----> ", les premières lettres des noms de villes doivent se trouver sur une même verticale.*/
  
+ SELECT CONCAT(service,"  ----->  " , ville)
+FROM serv;
+
+
+/* 82 : Sélectionner les employés en remplaçant les noms par '*****' dans le service n°1, trier le résultat suivant
+le N° de service.*/
+UPDATE emp 
+SET nom = '*****' 
+where noserv = 1;
+
+SELECT * 
+from emp 
+where noserv = 1;
+
+
+/*84 : Sélectionner les employés embauchés en 1988.*/
+
+SELECT * 
+from emp 
+where DATE_FORMAT(embauche , '%Y') = 1988;
+ 
+ /*86 : Sélectionner les positions des premiers M et E dans les noms des employés*/
+ 
+ SELECT nom , LOCATE('M', nom) , LOCATE('E', nom)  
+ from emp ; 
+ 
+ /*88 : Tracer un Histogramme des salaires avec nom, emploi, salaire triés dans l'ordre décroissant (max de
+l’histogramme avec 30 caractères).*/
+
+SELECT floor(sal / 30) * 30 as salaire, count(*)
+from emp
+group by salaire
+order by salaire DESC;
+
+/*90 : Même chose en écrivant la colonne embauche sous la forme ‘dd-mm-yy’, renommée
+embauche.*/
+
+SELECT nom , emploi , DATE_FORMAT(embauche, "%d %M  %Y") as embauche
+from emp 
+where noserv = 6;
+
+/*92 : Même chose en écrivant la colonne embauche sous la forme ‘day dd month(abv) yy'*/
+
+SELECT nom , emploi , DATE_FORMAT(embauche, "%W %d %b  %y") as embauche
+from emp 
+where noserv = 6;
+
+
+/*94 : Même chose en écrivant la colonne embauche sous la forme ‘Day-dd-Month-yyyy'*/
+
+SELECT nom , emploi , DATE_FORMAT(embauche, "%W-%d-%M-%Y") as embauche
+from emp 
+where noserv = 6;
+
+/*96 : Sélectionner les employés avec leur ancienneté en mois dans l'entreprise.*/
+
+SELECT TIMESTAMPDIFF(MONTH, embauche , NOW()) 
+from emp;
+
+
+/*98 : Sélectionner les employés ayant plus de 12 ans d’ancienneté.*/
+
+SELECT * , TIMESTAMPDIFF(YEAR, embauche , NOW()) as ANCIENNETE 
+from emp 
+where TIMESTAMPDIFF(YEAR, embauche , NOW()) > 12
+order by ANCIENNETE ASC;
+
+
+/*100 : Depuis combien de mois êtes-vous nés ?*/
+
+SELECT TIMESTAMPDIFF(MONTH, "1995-12-26" , NOW());
+
+
+/*102 : Paramétrer la requête qui précède sur l’emploi.*/
+SELECT emploi , ROUND(avg(sal + COALESCE(comm , 0)) , 2) as revenu_moyen 
+from emp 
+group by emploi;
+
+/*104 : Afficher le plus haut salaire, le plus bas salaire, la différence entre les deux.*/
+SELECT MAX(sal) as salaire_max , MIN(sal) as salaire_min , (MAX(sal)-MIN(sal)) as diff 
+from emp ;
+
+
+/*106 : Afficher le nombre de lettres du service dont le nom est le plus court.*/
+
+SELECT MIN(nom) as nom_min , LENGTH(MIN(nom)) as nombre_caractère 
+from emp; 
+
+
+/*108 : Déterminer le nombre d'employés du service 3 qui reçoivent éventuellement une
+commission.*/ 
+
+SELECT count(*) 
+from emp 
+where COALESCE(comm , 0) is not null  and noserv = 3;
+
+/*110 : Déterminer le nombre d'employés du service N°3.*/
+
+SELECT count(*) as nombre_employé 
+from emp 
+where noserv = 3;
+
+/*112 : Pour chaque service donner le salaire annuel moyen de tous les employés qui ne sont ni
+président, ni directeur.*/
+
+SELECT service, round(avg(sal + COALESCE(comm , 0)*12) , 2) as moy_revenu_annuel 
+from emp e , serv s 
+where e.noserv = s.noserv and emploi NOT IN("PRESIDENT" , "DIRECTEUR") 
+group by service;
+
+
+/*114 : Idem en remplaçant le numéro de service par le nom du service.*/
+
+SELECT service, emploi, count(*) nombre_employée, round(avg(sal + COALESCE(comm , 0)) , 2) as moy_salaire
+from emp e , serv s 
+where e.noserv = s.noserv  
+group by service , emploi; 
+
+
+/*116 : Sélectionner les services ayant au mois deux vendeurs.*/
+
+SELECT service , count(*) 
+from emp e , serv s 
+where emploi = "VENDEUR" and e.noserv = s.noserv 
+group by service 
+HAVING count(*) >= 2 ;
+
+/*118 : Sélectionner les emplois ayant un salaire moyen supérieur au salaire moyen des
+directeurs.*/
+
+SELECT emploi from emp 
+group by emploi
+having avg(sal + COALESCE(comm , 0)) > (SELECT avg(e2.sal + COALESCE(comm , 0)) from emp e2 where e2.emploi = "DIRECTEUR");
+
+
+/*120 : Afficher l'effectif, la moyenne et le total pour les salaires et les commissions par emploi.*/
+
+SELECT emploi , count(*) nombre_employé , avg(sal) moy_sal , avg(COALESCE(comm , 0)) moy_comm, sum(sal) sal_total , sum(COALESCE(comm , 0)) total_comm from emp
+group by emploi;
+
+
+/*122 : Insérez vous comme nouvel employé embauché aujourd’hui au salaire que vous
+désirez. Valider.*/
+
+INSERT into emp2 ( noemp , nom , prenom , emploi , sup , embauche , sal , comm , noserv ) 
+VALUES ( 1632 , "OLIVIER" , "QUENTIN" , "TECHNICIEN" , 1300 , NOW() , 13000 , 6000 , 2 );
+
+
+/*124 : Insérer le salarié dont le nom est MOYEN, prénom Toto, no 1010, embauché le 12/12/99,supérieur 1000, pas de comm, service no 1, salaire vaut le salaire moyen des employés.Valider.*/
+SET @salaire_moyen = (SELECT AVG(sal) FROM emp2);
+
+INSERT into emp2 ( noemp , nom , prenom , emploi , sup , embauche , sal , comm , noserv ) 
+VALUES ( 1010, "MOYEN" , "TOTO" , NULL , 1000 , "1999-12-12" , @salaire_moyen , NULL , 1 );
+
+
+
+/*128 : Vérifier que la table PROJ n’existe pas.*/
+
+SELECT * from proj;
+
+
+/*129 :
+ Créer une table PROJET avec les colonnes suivantes:
+numéro de projet (noproj), type numérique 3 chiffres, doit contenir une valeur. nom de projet (nomproj), type caractère, longueur = 10
+budget du projet (budget), type numérique, 6 chiffres significatifs et 2 décimales.*/
+
+CREATE TABLE projet
+(
+    noproj int(3) not null,
+    nomproj varchar(10),
+    budget DECIMAL(8,2)
+    );
+
+
+SELECT * from projet; 
+
+DESC proj;
+
+ALTER TABLE proj 
+RENAME to proj;
+
+
+/*130 :
+ Insérer trois lignes de données dans la table PROJ: numéros des projets = 101, 102,
+103
+noms des projets = alpha, beta, gamma budgets = 250000, 175000, 950000
+ Afficher le contenu de la table PROJ.
+ Valider les insertions faites dans la table PROJ.*/
+
+INSERT INTO proj(noproj , nomproj , budget)
+VALUES (101 , "alpha" , 250000);
+
+INSERT INTO proj(noproj , nomproj , budget)
+VALUES (102 , "beta" , 175000);
+
+INSERT INTO proj(noproj , nomproj , budget)
+VALUES (103 , "gamma" , 950000);
+
+
+/*132 :
+Ajouter une colonne NOPROJ (type numérique) à la table EMP.
+Afficher le contenu de la table EMP.*/
+
+ALTER TABLE emp add noproj INT(3);
+
+SELECT * from emp;
+
+
+/*134 : Affecter les employés dont le numéro est supérieur à 1350 au projet 102, sauf ceux qui sont déjà affectés à un projet.*/
+
+UPDATE emp 
+SET noproj = 102 
+where noemp > 1350 and noproj is NULL; 
+
+
+/*136 : Sélectionner les noms d'employés avec le nom de leur projet et le nom de leur
+service.*/
+
+SELECT nom , nomproj , service 
+from emp e , serv s , proj p 
+where e.noproj = p.noproj and e.noserv = s.noserv; 
+
+
+/*138 : Créer la vue EMP1 de la table EMP contenant les colonnes: numéro d'employé, nom et emploi et limitée aux employés du service numéro 1,*/
+CREATE VIEW emp1 AS
+SELECT noemp, nom, emploi
+FROM emp
+where noserv = 1;
+
+SELECT * from emp1;
+
+
+/*140 : Sélectionner les vendeurs de la vue emp1.*/ 
+
+SELECT * from emp1 
+where emploi = "VENDEUR";
+
+
+
+/*142 : Sélectionner la vue EMP1.*/
+
+SELECT * from emp1;
+
+
+
+
+
+
+
+
+
+
  
 
