f9779ff6fa455d5c77e2e2cab51ba7eb557b0ea8
nicodew2 <nicolas.deweireld@gmail.com>
2023-07-21 14:30:32 +0200

    recherche2

--- startjs/ajax/execice.ajax/solution2/main.js
+++ startjs/ajax/execice.ajax/solution2/main.js
@@ -1,15 +1,40 @@
 import { characters } from "./data.js";
 
-console.log(characters);
+// console.log(characters);
 
 const listepersonnage =document.querySelector("#personnages");
+
 const searchpersonnage =document.querySelector("#site-search");
 
-// for (let i = 0; i < characters.length; i++) {
-//     listepersonnage.innerHTML = 
-//         `<article><h2 class="name">${characters[i].name}</h2>
-//         <img src="./execice.ajax/Star-Wars-Search/solution2/images/${characters[i].imageLink}" alt="${characters.name}"></img>
-//         </article>`;
-//     }
+for (let i = 0; i < characters.length; i++) {
 
+    const personnage=document.createElement('article');
+    personnage.classList.add('personnage');
+    personnage.innerHTML= `<h2 class="name">${characters[i].name}</h2>
+        <img src="./images/${characters[i].imageLink}" alt="${characters[i].name}" class="portrait"></img>
+        `;
+
+        listepersonnage.appendChild(personnage);
+
+    }
+
+    searchpersonnage.addEventListener('keyup',e => {
+        const searchletter=e.target.value;
+        const personnages=document.querySelectorAll('.personnage');
+        filterElements(personnages,searchletter);
+    
+    });
+    
+    function filterElements(personnages,searchletter) {
+    
+        if(searchletter.length>0){
+            for(let i=0;i<personnages.length;i++){
+                if(personnages[i].textContent.toLowerCase().includes(searchletter)){
+                    personnages[i].style.display='block';
+                }else{
+                        personnages[i].style.display='none';
+                }
+            }
+        }
+    }
 
