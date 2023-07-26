af282c908ad3e8bf97654e1ca4adde77ef435958
unknown <david.wils591@gmail.com>
2023-07-26 10:38:51 +0200

    routeur react

--- demo-react/src/App.jsx
+++ demo-react/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import ComposantParent from './components/ComposantParent'
-import ComposantEvent from './components/ComposantEvent'
-import ComposantInput from './components/ComposantInput'
-import Garage from './components/Garage'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Profile from './pages/Profile';
+
+// import './App.css'
+// import ComposantParent from './components/ComposantParent'
+// import ComposantEvent from './components/ComposantEvent'
+// import ComposantInput from './components/ComposantInput'
+// import Garage from './components/Garage'
 
 function App() {
   
+return(
+  <div className="App">
+     <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/service" element = {<Services/>} />
+      <Route path="/profile" element = {<Profile/>} />
+     </Routes>
+  </div>
+);
+/*
   // const [prenom, setPrenom] = useState("David");
   // const [age, setAge] = useState(36);
 
@@ -14,17 +28,17 @@ function App() {
   //   setPrenom("Matthieu");
   // }
 
-  return (
-    <>
-      {/* <h1>Hello {prenom} !!!</h1>
-      <h1>Vous avez {age} ans</h1> */}
-      {<ComposantParent/>
-      // <ComposantEvent/>
-      // <ComposantInput/> 
-                          }
-      <Garage/>
-    </>
-  )
-}
+//   return (
+//     <>
+//       {/* <h1>Hello {prenom} !!!</h1>
+//       <h1>Vous avez {age} ans</h1> */}
+//       {<ComposantParent/>
+//       // <ComposantEvent/>
+//       // <ComposantInput/> 
+//                           }
+//       <Garage/>
+//     </>
+//   )
+// }*/
 
 export default App
